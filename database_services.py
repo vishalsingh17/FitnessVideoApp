76fbe185d782ab4625693c10232ff40b88202b48
unknown <vishal170997@gmail.com>
2022-06-08 13:18:51 +0530

    updated database_sevices.py

--- database_services.py
+++ database_services.py
@@ -20,8 +20,18 @@ def insert_data(workout_data):
 def delete_workout(workout_id):
     return db.delete(SCHEMA, TABLE, [workout_id])
 
-def get_all_workout(workout_id):
-    return db.sql(f'SELECT video_id, channel, title, duration FROM {SCHEMA}.{TABLE}')
+def get_all_workouts():
+    try:
+        return db.sql(f'SELECT video_id, channel, title, duration FROM {SCHEMA}.{TABLE}')
+    except harperdb.exceptions.HarperDBError:
+        return []
 
+def get_workout_today():
+    return db.sql(f'SELECT * FROM {SCHEMA}.{TABLE_TODAY} where id = 0')
 
+def update_workout_today(workout_data, insert=False):
+    workout_data['id'] = 0
+    if insert:
+        return db. insert(SCHEMA, TABLE_TODAY, [workout_data])
+    return db.update(SCHEMA, TABLE_TODAY, [workout_data])
 
