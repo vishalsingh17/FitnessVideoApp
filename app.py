fe0f323bfa6a1dd4e26714e73917663fc3f26509
unknown <vishal170997@gmail.com>
2022-06-08 15:27:39 +0530

    updated app.py

--- app.py
+++ app.py
@@ -3,4 +3,23 @@ import streamlit as st
 import database_services as dbs
 from yt_extractor import get_info
 
+@st.cache(allow_output_mutation=True)
+def get_workouts():
+    return dbs.get_all_workouts()
+
+def get_duration_text(duration_s):
+    seconds = duration_s % 60
+    minutes = int((duration_s/60)%60)
+    hours = int(duration_s/ (60*60)%24)
+    text = ''
+    if hours > 0:
+        text += f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+    else:
+        text += f'{minutes:02d}:{seconds:02d}'
+    return text
+
 st.title('WORKOUT APP')
+
+menu_options = ("Today's Workout", "All Workouts", "Add Workout")
+selection = st.sidebar.selectbox("Menu", menu_options)
+
